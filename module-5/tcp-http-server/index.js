3b487645562fde8e989e6248750877682ef2a383
Ryan Gaus <rsg1egoman@gmail.com>
2019-05-10 06:45:05 -0400

    Add students and assignments resources

--- module-5/tcp-http-server/index.js
+++ module-5/tcp-http-server/index.js
@@ -2,6 +2,7 @@ const app = require('express')();
 const net = require('net');
 const path = require('path');
 const uuid = require('uuid');
+const fs = require('fs');
 
 // TCP SERVER
 
@@ -84,7 +85,13 @@ app.use((req, res, next) => {
     data += d.toString();
   });
   req.on('end', d => {
-    req.body = data;
+    if (data.trim().length > 0) {
+      try {
+        req.body = JSON.parse(data);
+      } catch (err) {
+        res.status(400).send({detail: 'The request body can not be parsed as json.'})
+      }
+    }
     next();
   });
 });
@@ -180,6 +187,159 @@ app.get('/number-list', (req, res) => res.status(200).send({
     26,
   ],
 }));
+
+// Day 7: REST API design
+function getFile(filename) {
+  try {
+    const contents = fs.readFileSync(filename);
+    return JSON.parse(contents);
+  } catch (e) {
+    return [];
+  }
+}
+function saveFile(filename, data) {
+  fs.writeFileSync(filename, JSON.stringify(data));
+}
+const STUDENTS_FILE = '/tmp/students.json';
+const ASSIGNMENTS_FILE = '/tmp/assignments.json';
+const STUDENTS = getFile(STUDENTS_FILE);
+const ASSIGNMENTS = getFile(ASSIGNMENTS_FILE);
+
+function studentsCreate(req, res) {
+  const student = {...req.body, id: uuid.v4()};
+  STUDENTS.push(student);
+  res.status(201);
+  res.send(student);
+
+  saveFile(STUDENTS_FILE, STUDENTS);
+}
+app.post('/students', studentsCreate);
+function studentsReadByIdAssignmentsCreate(req, res) {
+  const assignment = {...req.body, id: uuid.v4(), studentId: req.params.id};
+  ASSIGNMENTS.push(assignment);
+  res.status(201);
+  res.send(assignment);
+
+  saveFile(ASSIGNMENTS_FILE, ASSIGNMENTS);
+}
+app.post('/students/:id/assignments', studentsReadByIdAssignmentsCreate);
+
+// Read
+function studentsReadList(req, res) {
+  res.status(200);
+  res.send({results: STUDENTS});
+}
+app.get('/students', studentsReadList);
+function studentsReadById(req, res) {
+  for (let i = 0; i < STUDENTS.length; i++) {
+    const student = STUDENTS[i];
+    if (student.id === req.params.id) {
+      res.status(200);
+      res.send(student);
+      return;
+    }
+  }
+  res.status(404);
+  res.send({detail: 'No such user with id '+req.params.id+' exists!'});
+}
+app.get('/students/:id', studentsReadById);
+function studentsReadByIdAssignmentsList(req, res) {
+  const assignments = [];
+  for (let i = 0; i < ASSIGNMENTS.length; i++) {
+    const assignment = ASSIGNMENTS[i];
+    if (assignment.studentId === req.params.id) {
+      assignments.push(assignment);
+    }
+  }
+  res.send({results: assignments});
+  res.end();
+}
+app.get('/students/:id/assignments', studentsReadByIdAssignmentsList);
+function studentsReadByIdAssignmentsReadById(req, res) {
+  for (let i = 0; i < ASSIGNMENTS.length; i++) {
+    const assignment = ASSIGNMENTS[i];
+    if (assignment.id === req.params.aid && assignment.studentId === req.params.id) {
+      res.status(200);
+      res.send(assignment);
+      return;
+    }
+  }
+  res.status(404);
+  res.send({detail: 'No such assignment with student id '+req.params.id+' and assignment id '+req.params.aid+' exists!'});
+}
+app.get('/students/:id/assignments/:aid', studentsReadByIdAssignmentsReadById);
+
+// Update
+function studentsUpdate(req, res) {
+  for (let i = 0; i < STUDENTS.length; i++) {
+    if (STUDENTS[i].id === req.params.id) {
+      STUDENTS[i] = req.body;
+
+      res.status(200);
+      res.send(STUDENTS[i]);
+      return;
+    }
+  }
+  res.status(404);
+  res.send({detail: 'No such user with id '+req.params.id+' exists!'});
+
+  saveFile(STUDENTS_FILE, STUDENTS);
+}
+app.put('/students/:id', studentsUpdate);
+function studentsAssignmentsUpdate(req, res) {
+  for (let i = 0; i < ASSIGNMENTS.length; i++) {
+    const assignment = ASSIGNMENTS[i];
+    if (assignment.id === req.params.aid && assignment.studentId === req.params.id) {
+      ASSIGNMENTS[i] = req.body;
+      ASSIGNMENTS[i].id = req.params.aid;
+      ASSIGNMENTS[i].studentId = req.params.id;
+
+      res.status(200);
+      res.send(ASSIGNMENTS[i]);
+      return;
+    }
+  }
+  res.status(404);
+  res.send({detail: 'No such assignment with student id '+req.params.id+' and assignment id '+req.params.aid+' exists!'});
+
+  saveFile(ASSIGNMENTS_FILE, ASSIGNMENTS);
+}
+app.put('/students/:id/assignments/:aid', studentsAssignmentsUpdate);
+
+// Delete
+function studentsDelete(req, res) {
+  for (let i = 0; i < STUDENTS.length; i++) {
+    if (STUDENTS[i].id === req.params.id) {
+      STUDENTS.splice(i, 1);
+
+      res.status(204);
+      res.end();
+      return;
+    }
+  }
+  res.status(404);
+  res.send({detail: 'No such user with id '+req.params.id+' exists!'});
+
+  saveFile(STUDENTS_FILE, STUDENTS);
+}
+app.delete('/students/:id', studentsDelete);
+function studentsAssignmentsDelete(req, res) {
+  for (let i = 0; i < ASSIGNMENTS.length; i++) {
+    if (ASSIGNMENTS[i].id === req.params.aid && ASSIGNMENTS[i].studentId === req.params.id) {
+      ASSIGNMENTS.splice(i, 1);
+
+      res.status(204);
+      res.end();
+      return;
+    }
+  }
+  res.status(404);
+  res.send({detail: 'No such user with id '+req.params.id+' exists!'});
+
+  saveFile(ASSIGNMENTS_FILE, ASSIGNMENTS);
+}
+app.delete('/students/:id/assignments/:aid', studentsDelete);
+
 
 const PORT = parseInt(process.env.PORT || 5000, 10);
 app.listen(PORT);
