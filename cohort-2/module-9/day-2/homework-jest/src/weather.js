306349ea2ce82d26496897e45a98c51c66b852ef
de1ux <nathan@de1ux.com>
2021-07-27 08:54:32 -0600

    Prep for tonights homework

--- cohort-2/module-9/day-2/homework-jest/src/weather.js
+++ cohort-2/module-9/day-2/homework-jest/src/weather.js
@@ -1,10 +1,16 @@
 import axios from "axios";
 
-const getWeather = (city) => {
-    return axios.get(`https://de1ux.com/weather`);
+
+export const getWeather = async (city) => {
+    return axios.get(`https://de1ux-weather-backend.herokuapp.com/weather?city=${city}`)
+        .then(response => response.data);
 }
 
 export const shouldWearJacket = async (city) => {
     const weather = await getWeather(city);
-    console.log(weather);
+
+    // Part 1 -- if the weather is <50 farenheit, return True!
+    // Note: the de1ux-weather-backend API returns temperature in kelvins
+
+    return weather;
 }
--- cohort-2/module-9/day-2/homework-jest/src/weather.test.js
+++ cohort-2/module-9/day-2/homework-jest/src/weather.test.js
@@ -1,7 +1,14 @@
 import {shouldWearJacket} from "./weather";
 
-describe('weather', () => {
-    it('should return true', async () => {
+jest.setTimeout(100000);
+
+describe('shouldWearJacket', () => {
+    it('should return true if the weather is too cold', async () => {
+        // Part 2: mock the response of axios, emulate a cold temperature coming from the API
         expect(await shouldWearJacket("syracuse")).toEqual(true);
     })
+
+    // Part 3: write a test that mocks the response of axios, emulates a warm temperature coming from the API
+
+    // Part 4: (bonus) write a test that mocks the "getWeather" function (NOT axios), and returns a cold temperature
 })
