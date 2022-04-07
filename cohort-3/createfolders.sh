e6938d8045132284b3ef979376a3453baa02b44c
Jesse Peplinski <peplinski.jesse@gmail.com>
2022-04-07 09:34:38 -0400

    Add cohort 3 folder

---
+++ cohort-3/createfolders.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# A bash script to create folders with subfolders all in one go.
+# https://pietropassarelli.com/bash_script.html
+
+mkdir -p {week1,week2,week3,week4,week5,week6,week7,week8,week9,week10,week11,week12,week13,week14,week15,week16,week17,week18,week19,week20,week21,week22,week23,week24,}/{day1,day2,day3,day4}
+
+# run this with `bash createfolders.sh`
